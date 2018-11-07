4f86cf393d802d0cafbd213eba570cdf19f9df1b
GabrielMendonca <gabba.kumbuca@gmail.com>
2018-11-07 12:24:43 -0200

    icluido cabecalho para testes e terminal

---
+++ funcoesParaTeste/a_cabecalho_para_teste.h
@@ -0,0 +1,17 @@
+
+/*  Faz com que o comando de limpar a tela funcione em Lunix e Win
+    Usar o camando LIMPA_TERM para limpara tela do terminal.
+*/
+#if defined(_WIN32) || defined(WIN32)
+#define LIMPA_TERM system("cls");
+#else
+#define LIMPA_TERM system("clear");
+#endif
+
+/* PRINCIPAIS FUNÇÕES */
+
+
+/* 
+    Inicia a função para cadastrar uma palestra nova
+*/
+void cadastrarPalestra();
