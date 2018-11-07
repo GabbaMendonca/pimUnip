685a17220534677676ba4ec4ceae238906c956cc
Lucas Apolonio <43792070+Lucas-Apolonio@users.noreply.github.com>
2018-11-07 02:58:06 -0200

    Update and rename assentos.c to assentosReserva.c

--- funcoesParaFazer/assentos.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-//faca uma funcao onde mostre os assentos da palestra
-void editarPalestras(void){
-    //codigo aqui
-}
---
+++ funcoesParaFazer/assentosReserva.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+//faca uma funcao onde mostre os assentos da palestra e efetue a reserva de tal assento
+void editarPalestras(void){
+    //codigo aqui
+}
