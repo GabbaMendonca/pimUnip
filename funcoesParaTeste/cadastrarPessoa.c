ed37ae98d7f41a5e4e5361c8f0b28e8fa8cf5989
GabrielMendonca <gabba.kumbuca@gmail.com>
2018-11-07 11:37:15 -0200

    Alterando a estrutura da funcao cadastrar pessoas

--- funcoesParaTeste/cadastrarPessoa.c
+++ funcoesParaTeste/cadastrarPessoa.c
@@ -1,47 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #define SIZE 100
 #define DESC 500
 #define CP 12
-struct Pessoa{
-    char nome[SIZE];
-    char email[SIZE];
-    int idade;
-    char cpf[CP];
-    int deficiencia;
-    char descDef[SIZE];
-}; struct Pessoa pE[SIZE];
 
-void novaPessoa(void){
-    int i;
-    for(i=0;i<SIZE;i++){
-        struct Pessoa pE[i];
-        void cadastraPessoa(void);
-        cadastraPessoa();
-        break;
-    }
+
+
+/* inicializando estruturas de variaveis
+STRUCTS
+*/
+
+typedef struct 
+{
+    char nome [SIZE];
+    char email [SIZE];
+    int  idade ;
+    char cpf [CP] ;
+    int  deficiencia ;
+    char descDef [SIZE];
+}PESSOA;
+
+        // struct Pessoa pE[SIZE];
+
+/* Fim dos strucs */
+
+
+
+
+/* PROTOTIPOS DAS FUNÇÕES */
+
+    void inseriPessoa();
+    void inseriEmail();
+    void inseriIdade();
+    void inseriCpf();
+    void inseriDeficiencia();
+    void inseriDescDef();
+ 
+/* FIM DOS PROTOTIPOS DAS FUNÇÕES */
+
+
+
+
+
+/* Função principal - cadastrarPalestra - onde que ira manipular as outras funções */
+
+void main()
+{
+    PESSOA pessoa;
+
+    inseriPessoa(&pessoa);
+    inseriEmail(&pessoa);
+    inseriIdade(&pessoa);
+    inseriCpf(&pessoa);
+    inseriDeficiencia(&pessoa);
+    inseriDescDef(&pessoa);
 }
 
-void cadastraPessoa(void){
+// void novaPessoa(void)
+// {
+//     int i;
+
+//     for (i = 0; i < SIZE; i++)
+//     {
+//         struct Pessoa pE[i];
+
+//         void cadastraPessoa(void);
+//         cadastraPessoa();
+//         break;
+//     }
+// }
+
+void cadastraPessoa(void)
+{
     //struct Pessoa pE;
     printf("digite seu nome:");
-    fgets(pE[SIZE].nome,SIZE,stdin);
+    fgets(pE[SIZE].nome, SIZE, stdin);
 
     printf("digite seu email:");
-    fgets(pE[SIZE].email,SIZE,stdin);
+    fgets(pE[SIZE].email, SIZE, stdin);
 
     printf("digite sua idade:");
-    scanf("%d",&pE[SIZE].idade);
+    scanf("%d", &pE[SIZE].idade);
     fflush(stdin);
+
     printf("digite seu cpf:");
-    fgets(pE[SIZE].cpf,CP,stdin);
+    fgets(pE[SIZE].cpf, CP, stdin);
 
     printf("digite 1 se tiver deficiencia ou 2 caso nao tenha:");
-    scanf("%d",&pE[SIZE].deficiencia);
+    scanf("%d", &pE[SIZE].deficiencia);
     fflush(stdin);
-    if(pE[SIZE].deficiencia==1){
+
+    if (pE[SIZE].deficiencia == 1)
+    {
         printf("diga a sua deficiencia:");
         fgets(pE[SIZE].descDef, SIZE, stdin);
-    }else{
+    }
+    else
+    {
         printf("aguarde");
     }
-
 }
